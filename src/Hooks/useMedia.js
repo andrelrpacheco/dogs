6520b75e9bae00cc9dedb62f1b3ae8e45b00280f
Andre Pacheco <andrelrpacheco@gmail.com>
2020-09-28 22:15:19 -0300

    Custom hook para o tamanho da tela

---
+++ src/Hooks/useMedia.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const useMedia = (media) => {
+  const [match, setMatch] = React.useState(null)
+
+  React.useEffect(() => {
+    function changeMedia() {
+      const { matches } = window.matchMedia(media)
+      setMatch(matches)
+    }
+    window.addEventListener('resize', changeMedia)
+    return () => {
+      window.removeEventListener('resize', changeMedia)
+    }
+  }, [media])
+
+  return match
+}
+
+export default useMedia
